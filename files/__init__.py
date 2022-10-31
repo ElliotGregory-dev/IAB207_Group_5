322bcc482ea58c8d443f1fd31774e9b31d0df990
chemonxyz <114207312+chemonxyz@users.noreply.github.com>
2022-11-01 00:55:21 +1000

    Revert "Revert "Merge branch 'main' of https://github.com/ElliotGregory-dev/IAB207_Group_5""
    This reverts commit f3bdaf904f2f98a1d4175a6d1dd186d3a0f6e8a4.

---
+++ files/__init__.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap4
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+db = SQLAlchemy()
+
+def create_app():
+    app=Flask(__name__)
+    app.debug=True
+    app.secret_key='thisisasecretkey122'
+
+    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///storage.db'
+
+    db.init_app(app)
+
+    bootstrap = Bootstrap4(app)
+
+    login_manager = LoginManager()
+
+    login_manager.login_view='auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+
+    from .views import mainbp
+    app.register_blueprint(mainbp)
+
+    from .auth import bp
+    app.register_blueprint(bp)
+
+    @app.errorhandler(404)
+    def not_found(e):
+        return render_template('error.html'),404
+
+
+    return app
---
+++ files/views.py
@@ -0,0 +1,21 @@
+from flask import Blueprint,render_template
+from .models import User
+
+mainbp = Blueprint('main',__name__)
+
+@mainbp.route('/')
+def index():
+    u1 = User.query.filter_by(name="user_name").first()
+    return render_template('index.html')
+
+@mainbp.route('/event_details')
+def event_details():
+    return render_template('event_details.html')
+
+@mainbp.route('/booking_history')
+def booking_history():
+    return render_template('booking_history.html')
+
+@mainbp.route('/create_update')
+def create_update():
+    return render_template('create_or_update.html')
