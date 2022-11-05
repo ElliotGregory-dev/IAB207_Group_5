78bda5635e05a8e307559ed97698c89089e3eb6c
Elliot Gregory <elliot.gregory.work@gmail.com>
2022-11-05 11:07:11 +1000

    Added query to my events
    Added the data query to allow for the data to actually show events.

--- files/views.py
+++ files/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint,render_template, request, redirect, url_for
-from flask_login import login_required
+from flask_login import login_required, current_user
 from .models import User, Event
 from .forms import CreateEventForm
 
@@ -32,7 +32,8 @@ def all_events():
 @mainbp.route('/my_events')
 @login_required
 def my_events():
-    return render_template('my_events.html', data=Event)
+    event = Event.query.filter_by(owner_id = current_user.getUserID())
+    return render_template('my_events.html', data=event)
 
 @mainbp.route('/search')
 def search():
